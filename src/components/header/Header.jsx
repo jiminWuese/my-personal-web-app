29d1ef82e53a6581e77e2df1009a002383cbba00
Jimin Wuese <jiminwuese@gmail.com>
2023-01-10 20:22:36 +0100

    Completed header section

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,18 +1,32 @@
 import React from 'react'
 import './header.scss';
 import CTA from './CTA';
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials';
 
 const Header = () => {
   return (
     <header>
       <div className="container header__container">
-        <h5>Hello, I'm</h5>
-        <h1>Jimin, Simon Wuese</h1>
-        <h5 className='text-light'>Maths Tutor and Web Developer</h5>
-        <CTA />
+        <div className="header__left">
+          <div className="me">
+            <img src={ME} alt="" />
+          </div>
+        </div>
+        <div className="header__right">
+          <h5>Hello, I'm</h5>
+          <h1>Jimin, Simon <br /> Wuese</h1>
+          <h5 className="text-light">Maths Tutor and Web Developer</h5>
+          <CTA />
+        </div>
+
+        <a href="#contact" className="scroll__down">
+          Scroll Down
+        </a>
+        <HeaderSocials />
       </div>
     </header>
-  )
+  );
 }
 
 export default Header;
