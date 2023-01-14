6bbb6ba56851cac6dff19bd26920b9893b8e4f49
Jimin Wuese <jiminwuese@gmail.com>
2023-01-14 18:14:28 +0100

    Completed portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,10 +1,90 @@
-import React from 'react';
-import './portfolio.scss';
+import React from "react";
+import "./portfolio.scss";
+import img1 from "../../assets/uiPhone.png";
+import img2 from "../../assets/headerLandingPage.png";
 
 const Portfolio = () => {
   return (
-    <div>Portfolio</div>
-  )
-}
+    <section id="portfolio">
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
 
-export default Portfolio
+      <div className="container portfolio__container">
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img1} alt="" />
+          </div>
+          <h3>UI/UX Designs </h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://figma.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img2} alt="" />
+          </div>
+          <h3>UI/UX Designs </h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://figma.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img1} alt="" />
+          </div>
+          <h3>UI/UX Designs </h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://figma.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+        <article className="portfolio__item">
+          <div className="portfolio__item-image">
+            <img src={img2} alt="" />
+          </div>
+          <h3>UI/UX Designs </h3>
+          <div className="portfolio__item-cta">
+            <a href="https://github.com" className="btn" target="_blank">
+              Github
+            </a>
+            <a
+              href="https://figma.com"
+              className="btn btn-primary"
+              target="_blank"
+            >
+              Live Demo
+            </a>
+          </div>
+        </article>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import './services.scss';
-import { BsCheckLg } from 'react-icons/bs'
+import './services.scss'; 
+import { GiCheckMark } from 'react-icons/gi';
 
 const Services = () => {
   return (
@@ -16,23 +16,23 @@ const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Defining design system and conducting user research</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Designing user interfaces</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Collaborating with cross-functional teams</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Keeping up-to-date with industry trends</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Iterating and testing of design</p>
             </li>
           </ul>
@@ -44,23 +44,23 @@ const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Creating and implementing visual elements</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Building and maintaining user interactions</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Optimizing performance</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
-              <p>Collaboration with other teams and ensuring cross-browser compatibility</p>
+              <GiCheckMark className='service__list-icon'/>
+              <p>Ensuring cross-browser compatibility</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Iterating and testing of code</p>
             </li>
           </ul>
@@ -72,23 +72,23 @@ const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Database design and management</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Security</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Server-side logic and business rules</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>API development</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Scalability and performance</p>
             </li>
           </ul>
@@ -100,23 +100,23 @@ const Services = () => {
 
           <ul className='service__list'>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Planning and delivering instruction</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Assessing student learning</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Curriculum development</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Building and developing teaching and learning aids</p>
             </li>
             <li>
-              <BsCheckLg className='service__list-icon'/>
+              <GiCheckMark className='service__list-icon'/>
               <p>Professinoal Development</p>
             </li>
           </ul>
