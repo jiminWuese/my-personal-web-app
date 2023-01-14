64d12026c119871624eb7f5987b9277fe27fa528
Jimin Wuese <jiminwuese@gmail.com>
2023-01-14 09:53:06 +0100

    Completed about section

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -8,14 +8,15 @@ const About = () => {
   return (
     <section id="about">
       <h5>Get to know</h5>
+
       <h2>About Me</h2>
+
       <div className="container about__container">
         <div className="about__me">
-        <div className="about__me-image">
-          <img src={me2} alt="Me Working" />
+          <div className="about__me-image">
+            <img src={me2} alt="Me Working" />
+          </div>
         </div>
-        </div>
-        
 
         <div className="about__content">
           <div className="about__cards">
